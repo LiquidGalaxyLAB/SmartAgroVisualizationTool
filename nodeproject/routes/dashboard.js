b17f23a4ba7a58c545e086d6f36300b30717ec2f
calv00 <javicalvosaxo@gmail.com>
2017-05-31 16:03:26 +0200

    Testing new things. Probably it will be deleted.

--- nodeproject/routes/dashboard.js
+++ nodeproject/routes/dashboard.js
@@ -10,9 +10,26 @@ router.get('/', function(req, res) {
         headers: {}
     }, function (error, response, body){
         if(!error && response.statusCode == 200){
-            res.json(body);
+            //poll();
+            getting();
+            res.render('dashboard', { temperature: body});
+            console.log("function success");
         }
     })
 });
 
+/*
+function getting() {
+    request({
+        method: 'GET',
+        uri: 'http://130.206.121.247:5000/flasklistener',
+        headers: {}
+    }, function (error, response, body){
+        if(!error && response.statusCode == 200){
+            console.log("getting...");
+            setTimeout(function() {getting()}, 5000);
+        }
+    })
+}
+*/
 module.exports = router;
