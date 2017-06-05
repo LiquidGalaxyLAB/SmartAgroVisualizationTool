081281afbcf498f7f76c7d86e011cc567fb5fe0e
calv00 <javicalvosaxo@gmail.com>
2017-06-05 11:19:42 +0200

    Removed nodeproject. Started nodeapiserver to serve data from database platform by API.

--- nodeapiserver/app.js
+++ nodeapiserver/app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var dashboard = require('./routes/dashboard');
 
 var app = express();
 
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/dashboard', dashboard);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
