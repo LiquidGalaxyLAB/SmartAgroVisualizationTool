3d91969adf87dfaf1d923b378ec0d762545e7a14
calv00 <javicalvosaxo@gmail.com>
2017-07-06 17:47:59 +0200

    Finished POST call. Added GET and DELETE images/:id calls.

--- sensor-api/routes/images.js
+++ sensor-api/routes/images.js
@@ -5,10 +5,8 @@ var multer = require("multer");
 var crypto = require("crypto");
 var ExifImage = require('exif').ExifImage;
 
-var http = require('http').createServer(express);
-
 var mongoose = require('mongoose');
-var sensor = require('../models/sensor.js');
+var image = require('../models/image.js');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
@@ -20,7 +18,10 @@ router.use(function(req, res, next) {
 });
 
 router.get('/', function(req, res) {
-
+  image.find(function (err, images) {
+    if (err) return next(err);
+    res.json(images);
+  });
 });
 
 router.get('/:id', function(req, res) {
@@ -29,6 +30,15 @@ router.get('/:id', function(req, res) {
 
 /* POST call of a image upload. It saves the image on the public/images/ folder
 and gets its exif data to save it in database by the image data model.
+The gps exif data extracted by ExifImage method should look like this:
+gps:
+   { GPSVersionID: [ 2, 3, 0, 0 ],
+     GPSLatitudeRef: 'N',
+     GPSLatitude: [ 42, 30, 28.56567593480345 ],
+     GPSLongitudeRef: 'W',
+     GPSLongitude: [ 2, 36, 54.22588235294118 ],
+     GPSAltitude: 635.9818731117825,
+     GPSTrack: 313.52734375 },
 */
 router.post('/', multer({ dest: 'public/images/' }).single('upload'),
   function(req, res) {
@@ -40,17 +50,54 @@ router.post('/', multer({ dest: 'public/images/' }).single('upload'),
           if (error)
               console.log('Error: '+error.message);
             else
-              console.log(exifData); // Do something with your data!
+              var imageBody = {
+                originalName: req.file.originalname,
+                fileName: req.file.filename,
+                gpsLatitude: ConvertDMSToDD(
+                  exifData.gps.GPSLatitude[0], exifData.gps.GPSLatitude[1],
+                  exifData.gps.GPSLatitude[2], exifData.gps.GPSLatitudeRef
+                ),
+                gpsLongitude: ConvertDMSToDD(
+                  exifData.gps.GPSLongitude[0], exifData.gps.GPSLongitude[1],
+                  exifData.gps.GPSLongitude[2], exifData.gps.GPSLongitudeRef
+                ),
+                gpsAltitude: exifData.gps.GPSAltitude,
+              }
+              console.log(imageBody);
+              image.create(imageBody, function (err, post) {
+                if (err) console.log(err);
+                res.json(post);
+              });
           });
     } catch (error) {
     console.log('Error: ' + error.message);
     }
-    res.json(req.file.originalname);
+    //res.json(req.file.originalname);
 });
 
 router.delete('/:id', function(req, res) {
-
+  image.findById(req.params.id, function (err, image) {
+    if (err) {
+      return console.log(err);
+    } else {
+      image.remove(function (err, image) {
+        if (err) {
+          return console.log(err);
+        } else {
+          res.json({ message: 'Image ' + image.originalName + ' deleted' });
+        }
+      });
+    }
+  });
 });
 
+function ConvertDMSToDD(degrees, minutes, seconds, direction) {
+    var dd = degrees + minutes/60 + seconds/(60*60);
+
+    if (direction == "S" || direction == "W") {
+        dd = dd * -1;
+    } // Don't do anything for N or E
+    return dd;
+}
 
 module.exports = router;
